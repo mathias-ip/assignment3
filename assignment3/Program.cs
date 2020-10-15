273a46f90a348feb5953bfb61bb4d26bfa92862b
Mathias <mip@ruc.dk>
2020-10-15 13:36:23 +0200

    bhh

--- assignment3/Program.cs
+++ assignment3/Program.cs
@@ -6,8 +6,7 @@ namespace assignment3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("petersiqjwfioslnv World!");
-            Console.WriteLine("nanana Peter får ingenting frem");
+            Console.WriteLine("fuck jer dregne!!);
           
         }
     }
